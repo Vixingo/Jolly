1c70c53e381373923c913b19641f8aa0ce899e5b
Vixingo <nazmul.abbu@gmail.com>
2025-08-26 00:09:55 +0600

    color picker

--- src/components/ui/color-picker.tsx
+++ src/components/ui/color-picker.tsx
@@ -2,7 +2,7 @@ import { useState, useRef, useEffect } from 'react'
 import { Button } from './button'
 import { Input } from './input'
 import { Label } from './label'
-import { Popover, PopoverContent, PopoverTrigger } from './popover'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 import { cn } from '@/lib/utils'
 
 interface ColorPickerProps {
